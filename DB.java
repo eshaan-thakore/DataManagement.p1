27341cc9fdb35c8b929174ccfc99ef534b805a37
Eshaan Thakore <eshaan.thakore@gmail.com>
2026-01-23 18:58:01 -0600

    Add files via upload

---
+++ DB.java
@@ -0,0 +1,172 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+
+public class DB {
+  public static final int NUM_RECORDS = 10;
+  public static final int RECORD_SIZE = 71;
+
+  private RandomAccessFile Dinout;
+  private int num_records;
+  private String Id;
+  private String Experience;
+  private String Married;
+  private String Wage;
+  private String Industry;
+
+  public DB() {
+    this.Dinout = null;
+    this.num_records = 0;
+    this.Id = "ID";
+    this.Experience = "EXPERIENCE";
+    this.Married = "MARRIED";
+    this.Wage = "WAGE";
+    this.Industry = "INDUSTRY";
+  }
+
+  /**
+   * Opens the file in read/write mode
+   * 
+   * @param filename (e.g., input.data)
+   * @return status true if operation successful
+   */
+  public void  open(String filename) {
+    // Set the number of records
+    this.num_records = NUM_RECORDS;
+
+    // Open file in read/write mode
+    try {
+      this.Dinout = new RandomAccessFile(filename, "rw");
+    } catch (FileNotFoundException e) {
+      System.out.println("Could not open file\n");
+      e.printStackTrace();
+    }
+  }
+  
+  /** 
+   * Writes the data to the location specified by file parameter
+   *  
+   */
+  public void writeRecord(RandomAccessFile file, String Id, String Experience, String Married, String Wage, String Industry ) {
+    	//format input values to be put in record
+        this.Id = String.format("%-10s", Id.length() > 8 ? Id.substring(0, 8) : Id);
+        this.Experience = String.format("%-5s", Experience.length() > 3 ? Experience.substring(0, 3) : Experience);
+        this.Married = String.format("%-5s", Married.length() > 3 ? Married.substring(0, 3) : Married);
+        this.Wage = String.format("%-20s", Wage.length() > 18 ? Wage.substring(0, 18) : Wage);
+        this.Industry = String.format("%-30s", Industry.length() > 28 ? Industry.substring(0, 28) : Industry);
+	try {
+		file.writeBytes(this.Id + this.Experience + this.Married + this.Wage + this.Industry+"\n");
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}  
+  }
+  
+  /**Takes in record number of record to be changed as well as what values to overwrite with 
+   * 
+   *  
+   */ 
+  public void overwriteRecord(int record_num, String Id, String Experience, String Married, String Wage, String Industry ) {
+    if ((record_num >= 0) && (record_num < this.num_records)) {
+      try {
+        Dinout.seek(0); // return to the top of the file
+        Dinout.skipBytes(record_num * RECORD_SIZE);
+	//overwrite the specified record
+        writeRecord (Dinout, Id, Experience, Married, Wage, Industry);
+      } catch (IOException e) {
+        System.out.println("There was an error while attempting to overwrite a record from the database file.\n");
+        e.printStackTrace();
+      }
+    }
+	System.out.println("Record Successfully Overwritten");
+  }
+
+  /** Opens CSV file and creates new data file
+   * Reads CSV file attributes
+   * @throws IOException 
+   */
+  public void createDB(String filename) throws IOException {
+      RandomAccessFile Din = new RandomAccessFile(filename+".csv", "r");
+      RandomAccessFile Dout = new RandomAccessFile(filename+".data","rw"); 
+      String line;
+      while ((line = Din.readLine()) != null) {
+          String[] attribute = line.split(",");
+          writeRecord (Dout, attribute[0], attribute[1], attribute[2], attribute[3], attribute[4]);
+      }
+      Dout.close();
+  }
+
+  /**
+   * Close the database file
+   */
+  public void close() {
+    try {
+      Dinout.close();
+    } catch (IOException e) {
+      System.out.println("There was an error while attempting to close the database file.\n");
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Get record number n (Records numbered from 0 to NUM_RECORDS-1)
+   * 
+   * @param record_num
+   * @return values of the fields with the name of the field and
+   *         the values read from the record
+   */
+  public Record readRecord(int record_num) {
+    Record record = new Record();
+    String[] fields;
+    if ((record_num >= 0) && (record_num < this.num_records)) {
+      try {
+        Dinout.seek(0); // return to the top of the file
+        Dinout.skipBytes(record_num * RECORD_SIZE);
+        // parse record and update fields
+        fields = Dinout.readLine().split("\\s{2,}", 0);
+        record.updateFields(fields);
+      } catch (IOException e) {
+        System.out.println("There was an error while attempting to read a record from the database file.\n");
+        e.printStackTrace();
+      }
+    }
+    return record;
+  }
+
+  /**
+   * Binary Search by record id
+   * 
+   * @param id
+   * @return Record number (which can then be used by read to
+   *         get the fields) or -1 if id not found
+   */
+  public int binarySearch(String id) {
+    int Low = 0;
+    int High = NUM_RECORDS - 1;
+    int Middle = 0;
+    boolean Found = false;
+    Record record;
+
+    while (!Found && (High >= Low)) {
+      Middle = (Low + High) / 2;
+      record = readRecord(Middle);
+      String MiddleId = record.Id;
+
+      // int result = MiddleId[0].compareTo(id); // DOES STRING COMPARE
+      int result = Integer.parseInt(MiddleId) - Integer.parseInt(id); // DOES INT COMPARE of MiddleId[0] and id
+      if (result == 0)
+        Found = true;
+      else if (result < 0)
+        Low = Middle + 1;
+      else
+        High = Middle - 1;
+    }
+    if (Found) {
+      return Middle; // the record number of the record
+    } else
+      return -1;
+  }
+}
---
+++ TestDB.java
@@ -0,0 +1,148 @@
+import java.io.IOException;
+
+//-----------------------------------------------------
+// Example code to read from fixed length records (random access file)
+//-----------------------------------------------------
+
+public class TestDB {
+  static Record record;
+
+  public static void main(String[] args) throws IOException {
+
+    // calls constructor
+    DB db = new DB();
+
+    // creates database from csv file 
+    db.createDB("input");
+    
+    // opens "input.data"
+    db.open("input.data");
+
+    System.out
+        .println("------------- Testing readRecord ------------");
+
+    // Reads record 0
+    // Then prints the values of the 5 fields to the screen with the name of the
+    // field and the values read from the record, i.e.,
+    // id: 00003 experience: 3 married: no wages: 1.344461678 industry:
+    // Business_and_Repair_Service
+    int record_num = 0;
+    record = new Record();
+    record = db.readRecord(record_num);
+    if (!record.isEmpty())
+      System.out.println("RecordNum " + record_num + ": " + record.toString() + "\n\n");
+    else {
+      System.out.println("Could not get Record " + record_num);
+      System.out.println("Record out of range");
+    }
+
+    // Reads record 9 (last record)
+    record_num = DB.NUM_RECORDS - 1;
+    record = db.readRecord(record_num);
+    if (!record.isEmpty())
+      System.out.println("RecordNum " + record_num + ": " + record.toString() + "\n\n");
+    else {
+      System.out.println("Could not get Record " + record_num);
+      System.out.println("Record out of range");
+    }
+
+    // Reads record 5 (middle record)
+    record_num = DB.NUM_RECORDS/2;
+    record = db.readRecord(record_num);
+    if (!record.isEmpty())
+      System.out.println("RecordNum " + record_num + ": " + record.toString() + "\n\n");
+    else {
+      System.out.println("Could not get Record " + record_num);
+      System.out.println("Record out of range");
+    }
+
+    // Reads record -1 (out of range)
+    record_num = -1;
+    record = db.readRecord(record_num);
+    if (!record.isEmpty())
+      System.out.println("RecordNum " + record_num + ": " + record.toString() + "\n\n");
+    else {
+      System.out.println("Could not get Record " + record_num);
+      System.out.println("Record out of range");
+    }
+
+    // Reads record 10 (out of range)
+    record_num = DB.NUM_RECORDS;
+    record = db.readRecord(record_num);
+    if (!record.isEmpty())
+      System.out.println("RecordNum " + record_num + ": " + record.toString() + "\n\n");
+    else {
+      System.out.println("Could not get Record " + record_num);
+      System.out.println("Record out of range");
+    }
+
+    System.out.println("\n\n" + "------------- Testing overwriteRecord ------------");
+
+    // Overwrite the previously read middle record
+    record_num = DB.NUM_RECORDS/2;
+    db.overwriteRecord(record_num,"00006","9","yes","1.12345678901234567890","Programmer");
+
+    // Rereads record 5 (middle record) to show that it has been overwritten
+    record_num = DB.NUM_RECORDS/2;
+    record = db.readRecord(record_num);
+    if (!record.isEmpty())
+      System.out.println("RecordNum " + record_num + ": " + record.toString() + "\n\n");
+    else {
+      System.out.println("Could not get Record " + record_num);
+      System.out.println("Record out of range");
+    }
+
+    System.out.println("------------- Testing binarySearch ------------");
+
+    // Find record with id 42 (should not be found)// Find record 17
+    String ID = "42";
+    record_num = db.binarySearch(ID);
+    if (record_num != -1) {
+      record = db.readRecord(record_num);
+      System.out
+          .println(
+              "ID " + ID + " found at Record " + record_num + "\nRecordNum " + record_num + ": \n" + record.toString()
+                  + "\n\n");
+    } else
+      System.out.println("ID " + ID + " not found in our records\n\n");
+
+    // Find record with id 00000 (the first one in the file)
+    ID = "0000";
+    record_num = db.binarySearch(ID);
+    if (record_num != -1) {
+      record = db.readRecord(record_num);
+      System.out
+          .println(
+              "ID " + ID + " found at Record " + record_num + "\nRecordNum " + record_num + ": \n" + record.toString()
+                  + "\n\n");
+    } else
+      System.out.println("ID " + ID + " not found in our records\n\n");
+
+    // Find record with id 00015 (the last one in the file)
+    ID = "00015";
+    record_num = db.binarySearch(ID);
+    if (record_num != -1) {
+      record = db.readRecord(record_num);
+      System.out
+          .println(
+              "ID " + ID + " found at Record " + record_num + "\nRecordNum " + record_num + ": \n" + record.toString()
+                  + "\n\n");
+    } else
+      System.out.println("ID " + ID + " not found in our records\n\n");
+
+    // Find record with id 00006 (somewhere in the middle)
+    ID = "00006";
+    record_num = db.binarySearch(ID);
+    if (record_num != -1) {
+      record = db.readRecord(record_num);
+      System.out
+          .println(
+              "ID " + ID + " found at Record " + record_num + "\nRecordNum " + record_num + ": \n" + record.toString()
+                  + "\n\n");
+    } else
+      System.out.println("ID " + ID + " not found in our records\n\n");
+
+    // Close database
+    db.close();
+  }
+}
